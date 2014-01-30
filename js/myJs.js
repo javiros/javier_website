9b88700e4554081af0e3e3ea07c3ec6cc4a5cb6d
javiros <javier_rossetti1972@hotmail.com>
2014-01-30 22:55:38 +0000

    final beta code

--- js/myJs.js
+++ js/myJs.js
@@ -8,11 +8,49 @@ $("#experience").click(function() {
    scrollToAnchor('experience');
 });
 
+$("#myexperience").click(function() {
+   scrollToAnchor('experience');
+});
+
+$("#my_experience").click(function() {
+   scrollToAnchor('experience');
+});
+
 $("#about").click(function() {
    scrollToAnchor('about');
+});
+
+$("#about_me").click(function() {
+   scrollToAnchor('about');
+});
+
+$("#my_experience").click(function() {
+   scrollToAnchor('experience');
 });
 
 $("#home").click(function() {
    scrollToAnchor('home');
 });
+
+$("#portfolio").click(function() {
+   scrollToAnchor('portfolio');
+});
+
+$("#myportfolio").click(function() {
+   scrollToAnchor('portfolio');
+});
+
+$("#my_portfolio").click(function() {
+   scrollToAnchor('portfolio');
+});
+
+$("#contact").click(function() {
+   scrollToAnchor('contact');
+})
+
+$("#button_contact").click(function() {
+   scrollToAnchor('contact');
+})
+
+
 
